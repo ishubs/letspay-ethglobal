4e0c604f86500e5d2d3ede6cd3ff6268e4fe7792
shubham giri <41017859+ishubs@users.noreply.github.com>
2025-09-27 00:32:32 +0530

    add required libraries and styling.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis", // 👈 add global
+      },
+      plugins: [
+        // Avoid injecting Buffer to prevent duplicate declarations in prebundled deps
+        NodeGlobalsPolyfillPlugin({
+          buffer: false,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // Do not alias buffer to avoid global Buffer duplication
+      process: "process/browser",
+    },
+  },
 })
